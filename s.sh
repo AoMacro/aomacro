d7e464877dc80871ff2ebc247c594d0014a3c957
AoMacro <52313712+AoMacro@users.noreply.github.com>
2020-09-20 10:49:10 +0800

    Update s.sh

--- s.sh
+++ s.sh
@@ -3,9 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 clear
 rm -rf $0
-echo
-    echo -e "感谢使用 "\033[35m AoMacro \033[0m"一键SSR翻墙加速脚本"
-	echo;
+
 
 libsodium_file="libsodium-1.0.17"
 libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.17/libsodium-1.0.17.tar.gz"
